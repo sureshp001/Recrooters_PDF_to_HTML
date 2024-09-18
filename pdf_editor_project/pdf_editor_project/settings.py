dc09f35244f43d08a4e217a89fe4424b2d2d4ed0
SureshP406 <suresh.ece406@gmail.com>
2024-09-18 12:06:20 +0530

    update allowed hosts

--- pdf_editor_project/pdf_editor_project/settings.py
+++ pdf_editor_project/pdf_editor_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xu)*=42gu3uoixtd!eb9q+180q5han!#)uo+0(s7mtb(&*m289'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
